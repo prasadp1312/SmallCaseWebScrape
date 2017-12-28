1b540d0eda04c2ec91dd587def64b550423dae9b
prasadp1312 <prasadp1312@gmail.com>
2017-12-28 21:49:12 +0530

    Update smallcase.py

--- smallcase.py
+++ smallcase.py
@@ -4,7 +4,82 @@ from bs4 import BeautifulSoup
 import re
 import time
 
+start_time = time.time()
 print('Python is Awesome')
+
+URL = 'https://www.smallcase.com/discover?sortBy=Latest&count=100'
+
+# Start the WebDriver and load the page
+wd = webdriver.Chrome('C:\\Coding\\WebDriver\\chromedriver.exe')
+wd.set_page_load_timeout(30)
+wd.get(URL)
+#wd.maximize_window()
+html_page  = wd.page_source
+
+### Find Login Button & Click -- Opnes New Window
+#wd.find_element_by_xpath("//button[@class='btn btn-lg btn-secondary kite-login']").click()
+
+scName = []
+scLink = []
+
+soup = BeautifulSoup(html_page,'html.parser')
+#print(soup.prettify())
+
+allSc = soup.find_all(class_="name pull-left")
+for idx,sc in enumerate(allSc):
+    scName.append(sc.string)
+    print(scName[idx])
+
+allScLinks = soup.findAll('a', attrs={'href': re.compile("^/smallcase/")})
+
+for idx,link in enumerate(allScLinks):
+    scIndivLink = link.get('href')
+    scLink.append('https://www.smallcase.com/smallcase/stocks' + scIndivLink[10:20])
+    print(scLink[idx])
+
+print('Please Login Within 30 Seconds :')
+time.sleep(30)
+
+scSegments = []
+scStocks= []
+for allStkSc in range(len(scLink)):
+    print('########################################')
+    print(scName[allStkSc])
+    print('########################################')
+    wd.get(scLink[allStkSc])
+    html_page  = wd.page_source
+    #wd.quit()
+
+    soup = BeautifulSoup(html_page,'html.parser')
+    #print(soup.prettify())
+
+    allSc = soup.find_all(class_="value-wrap value text-success")
+    for idx,sc in enumerate(allSc):
+        scSegments.append(sc.string)
+        print(idx,sc.string)
+
+    print('$$$ Segment -->')
+    allSc = soup.find_all(class_="segment-name")
+    for idx,sc in enumerate(allSc):
+        scSegments.append(sc.string)
+        print(idx,sc.string)
+
+    print('\n$$$ Stocks --->')
+    allSc = soup.find_all("tip-cont")
+    for idx,sc in enumerate(allSc):
+        scStocks.append((sc.string))
+        print(idx,sc.string)
+
+stop_time = time.time()
+print('Time Taken = ' + str(int(stop_time - start_time - 20)) + ' Seconds to scrape ' + str(len(scStocks)) + ' stocks from ' + str(len(scName)) + ' Smallcases')
+repeats = [[x,scStocks.count(x)] for x in set(scStocks)]
+for stocks in repeats:
+    print(stocks)
+scStocks = list(set(scStocks))
+print('Non Duplicate Stocks : ' + str(len(scStocks)))
+for stocks in scStocks:
+    print(stocks)
+wd.quit()
 
 URL = 'https://www.screener.in/'
 
@@ -14,7 +89,7 @@ wd.set_page_load_timeout(30)
 wd.get(URL)
 #wd.maximize_window()
 ### Enter in search bar the stock name
-StockName = 'Sampre'
+StockName = scStocks[0]
 wd.find_element_by_xpath("//input[@class='form-control']").send_keys(StockName)
 time.sleep(1)
 wd.find_element_by_xpath("//li[@class='active']").click()
